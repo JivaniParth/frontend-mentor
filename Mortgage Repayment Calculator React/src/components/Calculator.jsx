da6d08cd5a515b38c47627ad1b1ec8ccaceeb8a1
JivaniParth <parthjivani1504@gmail.com>
2025-07-31 17:59:24 +0530

    initial setup of design

--- Mortgage Repayment Calculator React/src/components/Calculator.jsx
+++ Mortgage Repayment Calculator React/src/components/Calculator.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import calculatorSvg from "../assets/images/icon-calculator.svg";
 
 export default function Calculator() {
-  <>
-    <div className="heading"></div>
-    <div className="form-container">
-      <form className="calculator-form"></form>
-    </div>
-  </>;
+  return (
+    <>
+      <div className="heading-container">
+        <h2 className="heading">Mortgage Calculator</h2>
+        <p className="clear-all">Clear All</p>
+      </div>
+      <div className="form-container">
+        <form className="calculator-form">
+          <label htmlFor="mortgage-amount">Mortgage Amount</label>
+          <input type="number" name="mortgage-amount" id="mortgageAmount" />
+          <label htmlFor="mortgage-term">Mortgage Term</label>
+          <input type="number" name="mortgage-term" id="mortgageTerm" />
+          <label htmlFor="interest-rate">Interest Rate</label>
+          <input type="number" name="interest-rate" id="interestRate" />
+          <label htmlFor="mortgage-type">Mortgage Type</label>
+          <input type="radio" name="repayment" id="repayment" />
+          <label htmlFor="repayment-radio">Repayment</label>
+          <input type="radio" name="interest-only" id="interestOnly" />
+          <label htmlFor="interest-only-radio">Interest Only</label>
+          <button type="submit">
+            <img src={calculatorSvg} alt="Calculator icon" />
+            Calculate Repayments
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
--- Mortgage Repayment Calculator React/src/components/CalculatorDisplay.jsx
+++ Mortgage Repayment Calculator React/src/components/CalculatorDisplay.jsx
@@ -1,3 +1,17 @@
 import React from "react";
+import illustrationEmptySvg from "../assets/images/illustration-empty.svg";
 
-export default function CalculatorDisplay() {}
+export default function CalculatorDisplay() {
+  return (
+    <>
+      <div className="repayments-display-container">
+        <img src={illustrationEmptySvg} alt="Repayments" />
+        <h2 className="repayments-empty-heading">Results shown here</h2>
+        <p className="repayments-empty-body">
+          Complete the form and click "Calculate repayments" to see what your
+          monthly repayments would be.
+        </p>
+      </div>
+    </>
+  );
+}
